db9058b54c9a44cf6c667a0ab317524fd7bc7754
Jed Peek <jedpeek@yahoo.com>
2018-06-29 22:42:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,9 +12,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT COUNT(characters.species) AS species_count, characters.species FROM characters
-  GROUP BY characters.species
-  ORDER BY species_count DESC LIMIT 1"
+  "SELECT characters.species, COUNT(characters.species) AS species_count FROM characters
+   GROUP BY characters.species
+   ORDER BY species_count DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
