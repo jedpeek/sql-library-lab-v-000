4c70093156ad8facaa66ce896216fd6e3feb91c9
Jed Peek <jedpeek@yahoo.com>
2018-06-29 22:53:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,9 +25,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series
+  "SELECT series.title COUNT(characters.species) FROM series
   JOIN characters ON series.id = characters.series_id
-  COUNT(characters.species) AS species_count
+  AS species_count
   GROUP BY characters.species
   ORDER BY species_count DESC LIMIT 1
   "
