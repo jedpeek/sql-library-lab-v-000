3dbd7b018e74a9ce3817040d2c679dc43269359a
Jed Peek <jedpeek@yahoo.com>
2018-06-29 22:45:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,8 +18,8 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors
-  JOIN "
+  "SELECT authors.name, subgenres.name FROM books
+  JOIN subgenres ON series.subgenre_id =  "
 end
 
 def select_series_title_with_most_human_characters
